237e319f3cac23c48b5dc14e8c32dcd0d6c2f0fe
wai-chong <mihaliswcm@outlook.com>
2024-01-11 03:24:54 +0000

    index Update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import * as serviceWorker from "./service-worker";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 root.render(<App />);
-
-serviceWorker.register();
